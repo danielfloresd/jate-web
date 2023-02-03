5e7990d36a58fb1ec1f73ae4c48d85a860cb4ae8
Restyled.io <commits@restyled.io>
2023-02-03 01:01:18 +0000

    Restyled by prettier

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,16 +1,16 @@
-import './database';
-import '../css/style.css';
+import "./database";
+import "../css/style.css";
 
-import {Workbox} from 'workbox-window';
+import { Workbox } from "workbox-window";
 
-import Editor from './editor';
+import Editor from "./editor";
 
-const main = document.querySelector('#main');
-main.innerHTML = '';
+const main = document.querySelector("#main");
+main.innerHTML = "";
 
 const loadSpinner = () => {
-  const spinner = document.createElement('div');
-  spinner.classList.add('spinner');
+  const spinner = document.createElement("div");
+  spinner.classList.add("spinner");
   spinner.innerHTML = `
   <div class="loading-container">
   <div class="loading-spinner" />
@@ -21,15 +21,15 @@ const loadSpinner = () => {
 
 const editor = new Editor();
 
-if (typeof editor === 'undefined') {
+if (typeof editor === "undefined") {
   loadSpinner();
 }
 
 // Check if service workers are supported
-if ('serviceWorker' in navigator) {
+if ("serviceWorker" in navigator) {
   // register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
+  const workboxSW = new Workbox("/src-sw.js");
   workboxSW.register();
 } else {
-  console.error('Service workers are not supported in this browser.');
+  console.error("Service workers are not supported in this browser.");
 }
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const {InjectManifest} = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest
 // file.
@@ -9,20 +9,16 @@ const {InjectManifest} = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode : 'development',
-    entry : {main : './src/js/index.js', install : './src/js/install.js'},
-    output : {
-      filename : '[name].bundle.js',
-      path : path.resolve(__dirname, 'dist'),
+    mode: "development",
+    entry: { main: "./src/js/index.js", install: "./src/js/install.js" },
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
-    plugins : [
+    plugins: [],
 
-    ],
-
-    module : {
-      rules : [
-
-      ],
+    module: {
+      rules: [],
     },
   };
 };
